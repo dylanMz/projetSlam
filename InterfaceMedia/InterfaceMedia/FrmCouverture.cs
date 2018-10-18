4ff674b3fb80888680a52798229b677eb1c29323
hugo Zagorjewsky <Hugo.zagorjewsky@outlook.Fr>
2018-10-18 13:48:16 +0200

    yo

--- InterfaceMedia/InterfaceMedia/FrmCouverture.cs
+++ InterfaceMedia/InterfaceMedia/FrmCouverture.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework.Forms;
-using LibMedia.LaibrairieClasses;
+using LibMedia;
 
 namespace InterfaceMedia
 {
