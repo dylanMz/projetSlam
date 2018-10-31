f10de8d2794938a3ee47b017284a71eb595ad49f
francois pagnoux <francois.pagnoux@outlook.fr>
2018-10-31 15:24:19 +0100

    exemplaire

--- LibMedia/LibMedia/CRUD_Exemplaire.cs
+++ LibMedia/LibMedia/CRUD_Exemplaire.cs
@@ -50,7 +50,7 @@ namespace LibMedia
             unComdeSql.Parameters.Add(new MySqlParameter("etat", MySqlDbType.String));
             unComdeSql.Parameters["etat"].Value = WExempEtat;
 
-            unComdeSql.Parameters.Add(new MySqlParameter("Id", MySqlDbType.String));
+            unComdeSql.Parameters.Add(new MySqlParameter("Id", MySqlDbType.Int16));
             unComdeSql.Parameters["Id"].Value = wbdcode;
 
             unComdeSql.ExecuteNonQuery();
@@ -70,7 +70,7 @@ namespace LibMedia
             //  ouverture de la connexion avec la base
 
             MySqlCommand unComdeSql = new MySqlCommand();
-            unComdeSql.CommandText = "proc_insert_exemplaire";
+            unComdeSql.CommandText = "proc_update_exemplaire";
             unComdeSql.CommandType = System.Data.CommandType.StoredProcedure;
             unComdeSql.Connection = _connexion.getConnexion();
 
@@ -80,7 +80,7 @@ namespace LibMedia
             unComdeSql.Parameters.Add(new MySqlParameter("etat", MySqlDbType.String));
             unComdeSql.Parameters["etat"].Value = WExempEtat;
 
-            unComdeSql.Parameters.Add(new MySqlParameter("wcode", MySqlDbType.String));
+            unComdeSql.Parameters.Add(new MySqlParameter("wcode", MySqlDbType.Int16));
             unComdeSql.Parameters["wcode"].Value = wbdcode;
 
             unComdeSql.ExecuteNonQuery();
@@ -91,11 +91,105 @@ namespace LibMedia
         }
 
 
+        // recherche exemplaire avec l'état
+
+        public void recherche_exemplaire_etat( String WExempEtat)
+        {
+
+
+            _connexion.OuvrirConnexion();
+            //  ouverture de la connexion avec la base
+
+            MySqlCommand unComdeSql = new MySqlCommand();
+            unComdeSql.CommandText = "proc__exemplaire_recherche_etat";
+            unComdeSql.CommandType = System.Data.CommandType.StoredProcedure;
+            unComdeSql.Connection = _connexion.getConnexion();
+
+            unComdeSql.Parameters.Add(new MySqlParameter("etat", MySqlDbType.String));
+            unComdeSql.Parameters["etat"].Value = WExempEtat;
+
+           
+
+            unComdeSql.ExecuteNonQuery();
+
+            _connexion.closeConnexion();
+
+
+        }
+        // recherche exemplaire avec la reference exemplaire
+        public void recherche_exemplaire_ref(String WExempRef)
+        {
+
+
+            _connexion.OuvrirConnexion();
+            //  ouverture de la connexion avec la base
+
+            MySqlCommand unComdeSql = new MySqlCommand();
+            unComdeSql.CommandText = "proc_recherche_exemp_refexemp";
+            unComdeSql.CommandType = System.Data.CommandType.StoredProcedure;
+            unComdeSql.Connection = _connexion.getConnexion();
+
+            unComdeSql.Parameters.Add(new MySqlParameter("refexemp", MySqlDbType.String));
+            unComdeSql.Parameters["refexemp"].Value = WExempRef;
 
 
 
+            unComdeSql.ExecuteNonQuery();
+
+            _connexion.closeConnexion();
 
 
+        }
+        // recherche exemplaire avec code livre
+        public void recherche_exemplaire_code(int wbdcode)
+        {
+
+
+            _connexion.OuvrirConnexion();
+            //  ouverture de la connexion avec la base
+
+            MySqlCommand unComdeSql = new MySqlCommand();
+            unComdeSql.CommandText = "proc_recherche_exemp_codelivre";
+            unComdeSql.CommandType = System.Data.CommandType.StoredProcedure;
+            unComdeSql.Connection = _connexion.getConnexion();
+
+            unComdeSql.Parameters.Add(new MySqlParameter("wcode", MySqlDbType.Int16));
+            unComdeSql.Parameters["wcode"].Value = wbdcode;
+
+
+
+            unComdeSql.ExecuteNonQuery();
+
+            _connexion.closeConnexion();
+
+
+        }
+
+
+        // supprimer un exemplaire 
+        public void delete_exemplaire(String WexempEtat)
+        {
+
+
+            _connexion.OuvrirConnexion();
+            //  ouverture de la connexion avec la base
+
+            MySqlCommand unComdeSql = new MySqlCommand();
+            unComdeSql.CommandText = "proc_delete_exemplaire";
+            unComdeSql.CommandType = System.Data.CommandType.StoredProcedure;
+            unComdeSql.Connection = _connexion.getConnexion();
+
+            unComdeSql.Parameters.Add(new MySqlParameter("refexemp", MySqlDbType.String));
+            unComdeSql.Parameters["refexemp"].Value = WexempEtat;
+
+
+
+            unComdeSql.ExecuteNonQuery();
+
+            _connexion.closeConnexion();
+
+
+        }
 
 
 
